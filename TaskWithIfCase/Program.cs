c4eb91038a07c98c82df9b945a3b5f13add7901f
KenanAkbarli <tu6m5m78u@code.edu.az>
2022-10-30 21:39:54 +0400

    Add project files.

---
+++ TaskWithIfCase/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int score = int.Parse(Console.ReadLine());
+if (score >=88 && score <= 100)
+{
+    Console.WriteLine($"Success-AA\nYour score:{score}");
+}
+else if (score >= 81 && score <= 87)
+{
+    Console.WriteLine($"Success-BA\nYour score:{score}");
+}
+else if (score >= 74 && score <= 80)
+{
+    Console.WriteLine($"Success-BB\nYour score:{score}");
+}
+else if (score >= 67 && score <= 73)
+{
+    Console.WriteLine($"Success-CB\nYour score:{score}");
+}
+else if (score >= 60 && score <= 66)
+{
+    Console.WriteLine($"Success-CC\nYour score:{score}");
+}
+else if (score >= 0 && score <= 59)
+{
+    Console.WriteLine($"Fail-FF\nYour score:{score}");
+}
+else
+{
+    Console.WriteLine("Invalid score");
+}
